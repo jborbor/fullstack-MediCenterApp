c2eee777e38b8fed3599108ff93d2b0d29ae73f8
Jonathan Borbor <jonathanbor2000@hotmail.com>
2025-08-18 20:07:50 -0500

    main-feat: project start, repository setup

---
+++ medi-center-backend/src/main/java/com/project/medi_center_backend/MediCenterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.medi_center_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediCenterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediCenterBackendApplication.class, args);
+	}
+
+}
---
+++ medi-center-backend/src/test/java/com/project/medi_center_backend/MediCenterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.medi_center_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediCenterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
